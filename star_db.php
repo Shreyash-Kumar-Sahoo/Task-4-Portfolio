7bf7871d1cf8755983cc16b8921259dd40cbab5f
Shreyash Kumar Sahoo <153584941+Shreyash-Kumar-Sahoo@users.noreply.github.com>
2026-06-29 21:07:25 +0530

    Add files via upload

---
+++ star_db.php
@@ -0,0 +1,12 @@
+<?php
+// db.php
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'star_db';
+
+$conn = mysqli_connect($host, $username, $password, $database);
+if (!$conn) {
+    die("Database connection failed: " . mysqli_connect_error());
+}
+?>
